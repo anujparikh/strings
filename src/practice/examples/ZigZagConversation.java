7f9ae96ff85d146349d58c0dbc4c2e1e824d6623
Anuj Parikh <parikh_anuj@cat.com>
2019-11-25 23:26:55 -0600

    2nd approach for zigzag

--- src/practice/examples/ZigZagConversation.java
+++ src/practice/examples/ZigZagConversation.java
@@ -1,5 +1,8 @@
 package practice.examples;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this
  * P   A   H   N
@@ -60,5 +63,30 @@ public class ZigZagConversation {
             }
         }
         return resultBld.toString();
+    }
+
+    /**
+     * Approach2:
+     *  Using List of Strings representing different rows
+     */
+    public static String convert2(String s, int numRows) {
+        if (numRows == 1) return s;
+
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < Math.min(numRows, s.length()); i++)
+            rows.add(new StringBuilder());
+
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows.get(curRow).append(c);
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder ret = new StringBuilder();
+        for (StringBuilder row : rows) ret.append(row);
+        return ret.toString();
     }
 }
